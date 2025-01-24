e4b3ac76cabbb6b35c2f113b96e0b593f29322d8
Aryo Wahyu Nugroho <aryowahyu24@gmail.com>
2025-01-24 10:13:40 +0700

    Commit Sebelum Refactor Vue 3

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Foods from '../views/Foods.vue'
-import FoodDetail from '../views/FoodDetail.vue'
-import Keranjang from '../views/Keranjang.vue'
-import PesananSukses from '../views/PesananSukses.vue'
+import Barang from '../views/Barang.vue'
+import BarangDetail from '../views/BarangDetail.vue'
+import CreateBarang from '../views/CreateBarang.vue'
+//import PesananSukses from '../views/PesananSukses.vue'
 
 Vue.use(VueRouter)
 
@@ -16,24 +16,24 @@ Vue.use(VueRouter)
   },
   {
     path: '/barangs',
-    name: 'Foods',
-    component: Foods
+    name: 'Barang',
+    component: Barang
   },
   {
     path: '/barangs/:id',
-    name: 'FoodDetail',
-    component: FoodDetail
+    name: 'BarangDetail',
+    component: BarangDetail
   },
   {
     path: '/createbarang',
-    name: 'Keranjang',
-    component: Keranjang
+    name: 'CreateBarang',
+    component: CreateBarang
   },
-  {
+ /* {
     path: '/pesanan-sukses',
     name: 'PesananSukses',
     component: PesananSukses
-  },
+  },*/
   {
     path: '/about',
     name: 'About',
